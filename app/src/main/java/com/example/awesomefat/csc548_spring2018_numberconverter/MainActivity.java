6fb140165ae6216dcc51b09eedde6e81ee2a2cee
romana <firdouse.romana@cuw.edu>
2018-02-06 16:01:35 -0600

    RomanaFCSC548 HW#1

--- app/src/main/java/com/example/awesomefat/csc548_spring2018_numberconverter/MainActivity.java
+++ app/src/main/java/com/example/awesomefat/csc548_spring2018_numberconverter/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity
 {
     //made a change
     
-    private TextView bitsTV;
+    private TextView bitsTV, kbitsTV, mbitsTV, gbitsTV, tbitsTV, bytesTV, kbytesTV, mbytesTV, gbytesTV, tbytesTV;
     private EditText mbET;
 
     @Override
@@ -20,14 +20,53 @@ public class MainActivity extends AppCompatActivity
         this.setContentView(R.layout.activity_main);
 
         this.bitsTV = (TextView)this.findViewById(R.id.bitsTV);
+        this.kbitsTV = (TextView)this.findViewById(R.id.kbitsTV);
+        this.mbitsTV = (TextView)this.findViewById(R.id.mbitsTV);
+        this.gbitsTV = (TextView)this.findViewById(R.id.gbitsTV);
+        this.tbitsTV = (TextView)this.findViewById(R.id.tbitsTV);
+        this.bytesTV = (TextView)this.findViewById(R.id.bytesTV);
+
+        this.kbytesTV = (TextView)this.findViewById(R.id.kbytesTV);
+        this.mbytesTV = (TextView)this.findViewById(R.id.mbytesTV);
+        this.gbytesTV = (TextView)this.findViewById(R.id.gbytesTV);
+        this.tbytesTV = (TextView)this.findViewById(R.id.tbytesTV);
+
+
         this.mbET = (EditText)this.findViewById(R.id.mbET);
+    }
+
+    private String roundOffNumber(double num)
+    {
+        return String.format("%.7f", num);
     }
 
     public void clickMeButtonPressed(View v)
     {
         String sValue = this.mbET.getText().toString();
         int iValue = Integer.parseInt(sValue);
-        bitsTV.setText("" + (iValue + 5));
+
+        int mbits = iValue * 8;
+        int kbytes = iValue * 1024;
+        int kbits = kbytes * 8;
+        int bytes = kbytes * 1024;
+        int bits = bytes * 8;
+        double gbytes = (double)iValue / 1024;
+        double gbits = gbytes * 8;
+        double tbytes = (double)gbytes / 1024;
+        double tbits = tbytes * 8;
+
+       // bitsTV.setText("" + (iValue + 5));
+
+        bitsTV.setText("" +  bits + " bits");
+        kbitsTV.setText("" +  kbits  + " kb");
+        mbitsTV.setText("" +  mbits + " mb");
+        gbitsTV.setText("" +  roundOffNumber(gbits) + " gb");
+        tbitsTV.setText("" +  roundOffNumber(tbits) + " tb");
+        bytesTV.setText("" +  bytes + " bytes");
+        kbytesTV.setText("" +  kbytes + " kB");
+        mbytesTV.setText("" +  iValue + " MB");
+        gbytesTV.setText("" +  roundOffNumber(gbytes) + " GB");
+        tbytesTV.setText("" +  roundOffNumber(tbytes) + " TB");
 
     }
 }
